723b299b0a60b8bdbd96e8ce5af74f2def38bffb
Edward Mehr <s.r.mehr@gmail.com>
2012-02-03 16:00:22 -0800

    new key for grooveshar

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,7 +2,7 @@ class MainController < ApplicationController
 
   def song_id
     begin
-    c = Curl::Easy.perform("http://tinysong.com/s/#{CGI.escape params[:q].sub(" ","+")}?format=json&limit=32&key=186bd60f3a33be26da02d62d334bddf4")
+        c = Curl::Easy.perform("http://tinysong.com/s/#{CGI.escape params[:q].sub(" ","+")}?format=json&limit=32&key=6ac3d5b1d6d3cd0af4a54e32961edd58") # old key: 186bd60f3a33be26da02d62d334bddf4
     rescue
     end
     parsed_json = ActiveSupport::JSON.decode(c.body_str)
